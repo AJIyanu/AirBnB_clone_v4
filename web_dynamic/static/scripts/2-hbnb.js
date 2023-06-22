2cb70947252c845879707bdf7838be39d7aaa422
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-06-22 04:04:21 +0100

    sync

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    const $amenitylist = $("ul li");
+    const amenity = [];
+
+    $amenitylist.find('input[type="checkbox"]').on('change', function() {
+        if ($(this).is(':checked')) {
+        const checked = $(this).parent('li').text();
+        amenity.push(checked);
+        $('.amenities h4').text(amenity.join(' '));
+        console.log('checked', checked);
+    } else if ($(this).is(':not(:checked)')) {
+        const unchecked = $(this).parent('li').text();
+        amenity.splice(amenity.indexOf(unchecked), 1);
+        if (amenity.length === 0) {
+            $('.amenities h4').html('&nbsp;');
+        } else {
+        $('.amenities h4').text(amenity.join(' '));
+        }
+        console.log('not checked')}
+    });
+
+    fetch("http://127.0.0.1:5001/api/v1/status/")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
